1084ed798169028256e9015a751683ef0e794385
SirNaze <119911665+SirNaze@users.noreply.github.com>
2024-07-16 18:49:37 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import { validaInputs, validaTextAreas } from "./validaciones.js";
+
+
+const inputs = document.querySelectorAll("input");
+const textareas = document.querySelectorAll("textarea");
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    validaInputs(input.target);
+  });
+});
+
+textareas.forEach((textarea) => {
+  textarea.addEventListener("blur", (textarea) => {
+    validaTextAreas(textarea.target);
+  });
+});
+
+
+
